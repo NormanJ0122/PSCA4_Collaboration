c6b7d66e6f53cf7f1ee6d247f14ce73f55d17900
Justin Norman <99421178+NormanJ0122@users.noreply.github.com>
2022-02-10 15:50:19 -0500

    Delete 1stDCPromo.ps1

--- 1stDCPromo.ps1
+++
@@ -1,2 +0,0 @@
-Import-Module ADDSDeployment
-Install-ADDSForest -CreateDnsDelegation:$false -DatabasePath "C:\Windows\NTDS" -DomainMode "WinThreshold" -DomainName "Hunt.com" -DomainNetbiosName "HUNT" -ForestMode "WinThreshold" -InstallDns:$true -LogPath "C:\Windows\NTDS" -NoRebootOnCompletion:$false -SysvolPath "C:\Windows\SYSVOL" -Force:$true
