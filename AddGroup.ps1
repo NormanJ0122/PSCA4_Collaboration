a4030fa63668fa29d364f878dbbe2fa160e1c42d
Justin Norman <99421178+NormanJ0122@users.noreply.github.com>
2022-02-10 16:00:05 -0500

    Add files via upload

---
+++ AddGroup.ps1
@@ -0,0 +1,7 @@
+﻿$list = ("Research","Integration","Testing")
+
+ForEach ($i in $list) {
+    $Path = "ou=" + "$i" + ",ou=IT,dc=HUNT,dc=com"
+    New-ADGroup $i -GroupCategory Security -GroupScope Global -DisplayName $i -Path $Path 
+    Write-Host $i
+    }
---
+++ AddOU.ps1
@@ -0,0 +1,9 @@
+﻿$csvfile = "C:\Users\Administrator\Documents\Scripts\OUmap.csv"
+$OUs = Import-CSV $csvfile
+New-ADOrganizationalUnit IT -DisplayName IT
+
+ForEach ($i in $OUs) {
+    $OU = $i.OrganUnit
+    $Path = "ou=IT,dc=HUNT,dc=com"
+    New-ADOrganizationalUnit $OU -DisplayName $OU -Path $Path
+    }
