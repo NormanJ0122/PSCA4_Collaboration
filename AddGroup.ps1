cbf91414f1062759579ab43eb9a804830908e292
Justin Norman <99421178+NormanJ0122@users.noreply.github.com>
2022-02-10 15:50:46 -0500

    Delete AddGroup.ps1

--- AddGroup.ps1
+++
@@ -1,7 +0,0 @@
-﻿$list = ("Research","Integration","Testing")
-
-ForEach ($i in $list) {
-    $Path = "ou=" + "$i" + ",ou=IT,dc=HUNT,dc=com"
-    New-ADGroup $i -GroupCategory Security -GroupScope Global -DisplayName $i -Path $Path 
-    Write-Host $i
-    }
