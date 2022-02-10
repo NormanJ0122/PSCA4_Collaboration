f1f5c3b22ebaee2c59b298719b5db4f5f7895957
Justin Norman <99421178+NormanJ0122@users.noreply.github.com>
2022-02-10 15:50:51 -0500

    Delete AddOU.ps1

--- AddOU.ps1
+++
@@ -1,9 +0,0 @@
-﻿$csvfile = "C:\Users\Administrator\Documents\Scripts\OUmap.csv"
-$OUs = Import-CSV $csvfile
-New-ADOrganizationalUnit IT -DisplayName IT
-
-ForEach ($i in $OUs) {
-    $OU = $i.OrganUnit
-    $Path = "ou=IT,dc=HUNT,dc=com"
-    New-ADOrganizationalUnit $OU -DisplayName $OU -Path $Path
-    }
